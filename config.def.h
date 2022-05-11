1dbaa57f01b056c79d671e1d99afab51f48b3372
Dilip Chauhan <chauhandilip305@gmail.com>
2022-05-11 21:16:49 +0530

    Updated on 11 May 2022

--- config.def.h
+++ config.def.h
@@ -66,6 +66,18 @@ static double minlatency = 8;
 static double maxlatency = 33;
 
 /*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 0;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
+
+/*
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
  * attribute.
  */
